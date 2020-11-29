a9d430420b03bc5ffcabafa1b896e1f14ec7a648
Alexander Karlsson <st.alexander85@gmail.com>
2020-11-29 20:21:36 +0100

    Initial Commit

---
+++ Disease!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Disease!
+//
+//  Created by Alexander Karlsson on 2020-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

f10e666a0f512debf1d1f0563db2fa03c6cade86
Alexander Karlsson <st.alexander85@gmail.com>
2020-12-05 14:34:53 +0100

    Update model

---
+++ Disease!/Model/Symptom.swift
@@ -0,0 +1,13 @@
+//
+//  Disease.swift
+//  Disease!
+//
+//  Created by Alexander Karlsson on 2020-12-05.
+//
+
+import SwiftUI
+
+struct Symptom: Hashable, Codable, Identifiable {
+    var id: Int
+    var symptom: String
+}
